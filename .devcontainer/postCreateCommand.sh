567caa0f7a7f0dbf9c29c2f579c4540f30eadd04
andlogreg <120428961+andlogreg@users.noreply.github.com>
2024-11-19 22:02:39 +0000

    feat: add todo and improve postCreateCommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,13 @@
 #/usr/bin/bash
 set -e
 
-#For now this script is dummy
+sudo apt update
+sudo apt install vim -y
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm ./kubectl
+
+
 echo "Done"
